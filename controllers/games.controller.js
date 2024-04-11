714537f746c792d69de9fd963e7a91499200ff0c
Laura Vanessa Ibanez <canelita1242@gmail.com>
2024-04-11 14:55:24 -0500

    Cambios finales a controller, queda a revisión

--- controllers/games.controller.js
+++ controllers/games.controller.js
@@ -1,37 +1,122 @@
+import { sign } from "jsonwebtoken";
 
 
 export const getGames = async (req, res) => {
-    let data = await null;
-    res.status(200).json({msg:'Datos de los juegos', data: data});
+    try{
+        let data = await null;
+        res.status(200).json({
+            succes: true,
+            data: data
+        });
+    }catch(error){
+        res.status(500).json({
+            data:[],
+            msg: 'No hay servicio',
+            succes: false
+        });
+    }
 }
 
 export const getGamesById = async (req, res) => {
+    try{
     let{id} = req.params;
     let data = await null;
-    res.status(200).json({msg: 'Datos del juego', data: data});
+    if(!data){
+        throw new Error("Juego no encontrado")
+    }
+    res.status(200).json({
+        succes: true,
+        data: data
+    })
+    }catch(error){
+        res.status(204).json({
+            data: [],
+            msg: error.Error,
+            succes:false
+        });
+    }
+}
+
+export const getGamedByName = async(req,res) =>{
+    try{
+        //let {nombre de video juego} = req.params;
+        let data = await null;
+        if(!data){
+            throw new Error("Juego no encontrado")
+        }
+        res.status(200).json({
+            succes: true,
+            data: data
+        });
+        
+    }catch(error){
+        res.status(204).json({
+            data: [],
+            msg: error.Error,
+            succes: false
+        });
+    }
 }
 
 export const postGames = async (req, res) =>{
-    //let {nombres base de datos} = req.body;
-
-    let data = await null;
-    res.status(200).json({msg: 'Esto es un post', data: data});
+    try{
+        //let {nombres base de datos} = req.body;
+        let data = await null;
+        res.status(201).json({
+            succes: true,
+            data: data
+        });
+    }catch(error){
+        res.status(500).json({
+            data: [],
+            msg: "Servicio no disponible",
+            succes: false
+        });
+    }
 }
 
 export const putGames = async(req, res) =>{
-    //let {nombres base de datos} = req.body;
-    let{id} = req.params
-    let data = await null;
+    try{
+        //let {nombres base de datos} = req.body;
+        let{id} = req.params
+        let data = await null;
+        res.status(200).json({
+            succes: true,
+            data: data
+        });
+    }catch(error){
+        res.status(500).json({
+            data: [],
+            msg: "Servicio no disponible",
+            succes: false
+        });
+    }
+
+    
     res.status(200).json({msg: 'Esto es un put', data: data});
 }
 
 export const deleteGames = async(req, res) =>{
-
+    try{
+        let {id} = req.params;
+        let data = await null;
+        res.status(200).json({
+            succes: true,
+            data: data
+        });
+    }catch(error){
+        res.status(500).json({
+            data: [],
+            msg: "Servicio no disponible",
+            succes: false
+        })
+    }
 }
 
 export default{
     getGames,
     getGamesById,
+    getGamedByName,
     postGames,
     putGames,
     deleteGames
